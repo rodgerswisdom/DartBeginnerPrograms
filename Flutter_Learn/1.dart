c42464f6dfba1e5f35c9268b9834151c79e490dc
rodgerswisdom <trulyhawona@gmail.com>
2024-07-19 01:12:32 +0300

    Flutter Stateful widgets

---
+++ Flutter_Learn/1.dart
@@ -0,0 +1,17 @@
+// Stateful widget
+
+import 'package: flutter/material.dart';
+
+class StatefulTest extends StatefulWidget {
+
+@overide 
+_TestState createState() => _TestState();
+
+}
+
+class _TestState extends State<StatefulTest> {
+@overide
+Widget build(BuildContext context) {
+	return Container();
+}
+}
